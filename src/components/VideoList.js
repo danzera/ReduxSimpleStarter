6af6df58917141b32741cb269723a331b773d424
Dan Zera <dczera@gmail.com>
2017-11-14 20:12:40 -0600

    create base <VideoListItem /> and create an array of these components within <VideoList /> based on the current list of videos passed down from <App />

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import VideoListItem from './VideoListItem';
 
 const VideoList = (props) => {
+	// create array of <VideoListItem /> components to be rendered within <VideoList />
+	const videos = props.videos.map((video, index, videos) => {
+		return <VideoListItem key={video.etag} video={video} />
+	});
 
 	return (
 		<ul>
-			Greetings from VideoList!
+			{videos}
 		</ul>
 	);
 }
--- src/index.js
+++ src/index.js
@@ -50,7 +50,7 @@ class App extends Component {
 				<h1>YouTube Search-O-Matic 5000</h1>
 				<SearchBar onSearchTermChange={youTubeSearch} />
 				<VideoDetails selectedVideo={this.state.selectedVideo} />
-				<VideoList />
+				<VideoList videos={this.state.videos}/>
 			</div>
 		);
 	} // end render()
