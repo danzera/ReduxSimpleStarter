58f7bf6428f368aab5a7a3fd9ccfa28eb4edfe26
Dan Zera <dczera@gmail.com>
2017-11-14 20:55:14 -0600

    add structure/styling to <VideoListItem /> component

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -4,11 +4,14 @@ import VideoListItem from './VideoListItem';
 const VideoList = ({videos, onVideoSelect}) => {
 	// create array of <VideoListItem /> components to be rendered within <VideoList />
 	const videoListItems = videos.map((video, index, videos) => {
-		return <VideoListItem key={video.etag} video={video} onVideoSelect={selectedVideo => onVideoSelect(selectedVideo)} />
+		return <VideoListItem
+							key={video.etag}
+							video={video}
+							onVideoSelect={selectedVideo => onVideoSelect(selectedVideo)} />
 	});
 
 	return (
-		<ul>
+		<ul className="col-md-4 list-group">
 			{videoListItems}
 		</ul>
 	);
--- src/components/VideoListItem.js
+++ src/components/VideoListItem.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
 const VideoListItem = ({video, onVideoSelect}) => {
+	const imageUrl = video.snippet.thumbnails.default.url;
+	const videoTitle = video.snippet.title;
 	
 	return (
-		<li onClick={event => onVideoSelect(video)}>{video.snippet.title}</li>
+		<li className="list-group-item" onClick={event => onVideoSelect(video)}>
+			<div className="video-item media">
+				<div className="media-left">
+					<img className="media-object" src={imageUrl} />
+				</div>
+				<div className="media-body">
+					<div className="media-heading">{videoTitle}</div>
+				</div>
+			</div>
+		</li>
 	);
 }
 
